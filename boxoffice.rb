aabfffe4dc44083590fe0eb14227dd5f657cf9c1
LucasVillela <lb.villela@gmail.com>
2015-10-11 19:40:20 -0300

    Grossing List and movie info working

--- boxoffice.rb
+++ boxoffice.rb
@@ -4,26 +4,41 @@ require 'sinatra/static_assets'
 require 'shotgun'
 require 'nokogiri'
 require 'open-uri'
+require 'mysql2'
 require './boxofficecode'
 
 
 
 get '/' do
-  page = Nokogiri::HTML(open("http://www.boxofficemojo.com/alltime/world/?pagenum=1&p=.htm"))   
-  @td = page.css('td')
-  @ano = anoFilme(@td)
-  @nome = nomeFilme(@td)
-  @bilheteria = bilheteriaFilme(@td)
-
+  
+  @glist = grossingList
 
   erb :index
 end
 
 get '/test' do
 
-  url = 'http://omdbapi.com/?t=interstellar'
-  @result = filmeInfo(url)
-  @poster_path = posterFilme(@result['imdbID'])
+
   erb :test
   
 end
+
+
+=begin
+
+  result.each do |row|
+    @vetor << row["Title"]
+  end 
+
+
+<% @nome.each_with_index do |element,index| %> 
+    <tr>
+    <td><%=index+1%>
+    <td><%=@nome[index]%></td>
+    <td><%=@bilheteria[index]%></td>
+    <td><%=@ano[index]%></td>
+    </tr>
+    <%end%>
+    </table>
+
+=end
--- boxofficecode.rb
+++ boxofficecode.rb
@@ -1,69 +1,13 @@
-
-require 'open-uri'
-require 'json'
-require 'ruby-tmdb3'
+require 'mysql2'
 
 
+@@client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "root", :database => "movies")
 
-def filmeInfo(url)
+def grossingList
+  @client.class
+  result = @@client.query("SELECT Title,Rank,Grossing,MYear,IMDBRating,Actors,Plot,Genre,Director,Poster FROM filmes")
+  
 
-  #url = 'http://omdbapi.com/?t=Avatar'
-  buffer = open(url).read
-  result = JSON.parse(buffer)
   return result
+
 end
-
-def posterFilme(imdbid)
-
-  file = File.open("/home/lucas/Sinatra/BoxOffice_app/api","r")
-  api = file.read.chomp
-  Tmdb.api_key = api
-  file.close
-  Tmdb.default_language = "en"
-  movie = TmdbMovie.find(:imdb => imdbid, :limit => 1)
-
-  return movie.poster_path
-end
-
-
-  def anoFilme(td)
-    vetor = Array.new
-    td.each do |line|
-      if(line.text.to_i > 1900 && line.text.to_i < 2020 && line.css('b').text!='2012')      
-        if(line.text.include? "^")
-          vetor.push(line.text.chomp("^"))
-        else   
-          vetor.push(line.text)
-        end
-      end
-    end
-    return vetor
-  end
-
-  def nomeFilme(td)
-    regex = /[A-Z]+[a-zA-Z]+/
-    vetor = Array.new
-    td.css('b').each do |line|
-      var = line.text
-      if(vetor.length >= 100)
-        next
-      end
-      if((var.match(regex)) && (var != 'Worldwide') && (var != 'WORLDWIDE GROSSES'))
-        vetor.push(line.text)
-      end
-      if(var.to_i == 2012)
-        vetor.push(line.text)
-      end
-    end
-    return vetor
-  end
-
-  def bilheteriaFilme(td)
-    vetor = Array.new
-    td.css('b').each do |line|
-      if line.text.include? "$"
-        vetor.push(line.text)
-      end
-    end
-    return vetor
-  end
